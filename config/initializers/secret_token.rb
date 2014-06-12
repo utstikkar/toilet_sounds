d80aa7567a5e91a4b762fddecd00808fb42a5c67
utstikkar <amelie.anglade@gmail.com>
2014-06-12 19:31:20 +0200

    Add precompiled assets for Heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,9 +5,9 @@
 # no regular words or you’ll be exposed to dictionary attacks.
 # You can use `rake secret` to generate a secure secret key.
 # Make sure your secret_key_base is kept private # if you’re sharing your code publicly.
-require ’securerandom’
+require 'securerandom'
 def secure_token
-  token_file = Rails.root.join(’.secret’)
+  token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
     # Use the existing token.
     File.read(token_file).chomp
@@ -18,4 +18,4 @@ def secure_token
     token
   end 
 end
-SampleApp::Application.config.secret_key_base = secure_token
+ToiletSounds::Application.config.secret_key_base = secure_token
